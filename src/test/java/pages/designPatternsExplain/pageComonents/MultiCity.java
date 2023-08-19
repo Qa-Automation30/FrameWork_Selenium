4c958d0d6d10cc558917e757910683efc0da81fa
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-08-19 16:26:48 +0530

    Creating a frameWork

--- src/test/java/pages/designPatternsExplain/pageComonents/MultiCity.java
+++ src/test/java/pages/designPatternsExplain/pageComonents/MultiCity.java
@@ -5,6 +5,6 @@ import pages.designPatternsExplain.abstractComponent.SearchFlights;
 public class MultiCity implements SearchFlights {
     @Override
     public void checkAvailabilityFlights(String origin, String destination) {
-
+        System.out.println("I am in MultiCity trip");
     }
 }
